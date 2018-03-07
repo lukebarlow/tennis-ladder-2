bd7a02c35b5492c6a7ebad0b08de1b30e7720fef
Luke Barlow <luke.barlow@gmail.com>
2018-03-07 16:49:50 +0000

    fix up

--- src/client/index.js
+++ src/client/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import CssScene from './css3d/'
-// import PanoramaScene from './panorama/'
-// import Navigation from './navigation'
+import PanoramaScene from './panorama/'
+import Navigation from './navigation'
 
 import TopLinks from './components/TopLinks'
 
-// Navigation.setScenes([CssScene, PanoramaScene])
+Navigation.setScenes([CssScene, PanoramaScene])
 
 render(<TopLinks navigator={Navigation} />, document.getElementById('top-links'))
